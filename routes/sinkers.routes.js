beedbc39840b8b112863962bed2632ea20eb715e
Jonathan Bijl <sannoayo@gmail.com>
2023-08-03 19:52:09 -0400

    updates

--- routes/sinkers.routes.js
+++ routes/sinkers.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const sinkerController = require('../controllers/sinkers');
+const { authenticateUser } = require('../middleware/auth');
 
 // Routes for sinkers
 router.get('/', sinkerController.getAllSinkers);
 router.get('/:id', sinkerController.getSinkerById);
-router.post('/', sinkerController.createSinker);
-router.put('/:id', sinkerController.updateSinker);
-router.delete('/:id', sinkerController.deleteSinker);
+router.post('/', authenticateUser, sinkerController.createSinker);
+router.put('/:id', authenticateUser, sinkerController.updateSinker);
+router.delete('/:id', authenticateUser, sinkerController.deleteSinker);
 
 module.exports = router;
