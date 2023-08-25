04894084c7f20710b9c82eaaf4209b0bca95f064
Jonathan Bijl <sannoayo@gmail.com>
2023-08-24 21:04:03 -0400

    Organizational Cleanup models

--- models/user.js
+++ models/user.js
@@ -15,22 +15,4 @@ const userSchema = new mongoose.Schema({
   combos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Combo' }],
 });
 
-// Hash the password before saving it to the database
-// ust
-
-// Method to compare password for login
-// userSchema.methods.comparePassword = async function (password) {
-//   try {
-//     return await bcrypt.compare(password, this.password);
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// // Method to generate a JWT token for the user
-// userSchema.methods.generateToken = function () {
-//   const token = jwt.sign({ userId: this._id }, jwtSecret, { expiresIn: '1h' });
-//   return token;
-// };
-
 module.exports = mongoose.model('User', userSchema);
